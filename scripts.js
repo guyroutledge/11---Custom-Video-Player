761323a5ea4abfe5f0d5a7483d8fe4ffb11de280
Guy Routledge <mail@guyroutledge.co.uk>
2019-10-24 09:42:53 +0100

    Pseudo code for scrub method

--- scripts.js
+++ scripts.js
@@ -21,27 +21,40 @@ class VideoPlayer {
 		const controlType = e.target.name;
 		this.video[controlType] = e.target.value;
 	}
-	handleProgress = e => {
+	handleProgress = () => {
 		const { video, progressBar } = this;
 		const progress = ( video.currentTime / video.duration ) * 100;
 		progressBar.style.flexBasis = progress + '%';
+	}
+	scrub = e => {
+		// get current click/mouse position
+		// calculate distance from left edge of player
+		// calculate percentage of player width
+		// update video currentTime -> auto update progress
+		console.log( e );
 	}
 	init() {
 		const {
 			video,
 			skipButtons,
+			progress,
 			ranges,
 			togglePlay,
 			toggleIcon,
 			skip,
 			handleRangeUpdate,
-			handleProgress
+			handleProgress,
+			scrub
 		} = this;
 
 		video.addEventListener( 'click', togglePlay );
 		video.addEventListener( 'play', toggleIcon );
 		video.addEventListener( 'pause', toggleIcon );
 		video.addEventListener( 'timeupdate', handleProgress );
+
+		progress.addEventListener( 'click', scrub );
+		progress.addEventListener( 'mousemove', scrub );
+
 		skipButtons.forEach( button => button.addEventListener( 'click', skip ) );
 		ranges.forEach( range => range.addEventListener( 'change', handleRangeUpdate ) );
 	}
