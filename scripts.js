b3ba35b7c5c8b45f4a146e65c43692add77314d1
Guy Routledge <mail@guyroutledge.co.uk>
2019-10-24 09:08:40 +0100

    Add method to toggle play icon

--- scripts.js
+++ scripts.js
@@ -10,9 +10,14 @@ class VideoPlayer {
 	togglePlay = () => {
 		this.video.paused ? this.video.play() : this.video.pause();
 	}
+	toggleIcon = e => {
+		const icon = this.video.paused ? '►' : '||';
+		e.target.textContent = icon;
+	}
 	init() {
 		this.video.addEventListener( 'click', this.togglePlay );
 		this.toggle.addEventListener( 'click', this.togglePlay );
+		this.toggle.addEventListener( 'click', this.toggleIcon );
 	}
 }
 const videoPlayer = new VideoPlayer();
